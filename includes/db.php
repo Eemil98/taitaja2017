e3e8e80e2981933bab9e92054573475d1988f784
Competitor1 <Competitor1>
2017-05-18 11:05:23 +0300

    db conf changes in db.php

--- includes/db.php
+++ includes/db.php
@@ -5,6 +5,17 @@
 	$dbuser = "azure";
 	$dbpass = "password";
 	$dbloc = "localhost:53189";
+
+	foreach ($_SERVER as $key => $value) {
+		if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+			continue;
+		}
+
+		$dbloc = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+		$db = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+		$dbuser = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+		$dbpass = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+	}
 	$mysql = mysqli_connect($dbloc, $dbuser, $dbpass, $db);
 /**/
 
